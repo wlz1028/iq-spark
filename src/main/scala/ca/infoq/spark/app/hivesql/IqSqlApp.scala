a75dcf7c5d384e8a903b33f715d04848f24205da
Lizhou Wang <wlz1028@gmail.com>
2016-08-27 22:18:36 -0400

    update config

--- src/main/scala/ca/infoq/spark/app/hivesql/IqSqlApp.scala
+++ src/main/scala/ca/infoq/spark/app/hivesql/IqSqlApp.scala
@@ -3,6 +3,8 @@ package ca.infoq.spark.app.hivesql
 import ca.infoq.spark.common.config.{IqApp, IqAppArgsParser}
 import ca.infoq.spark.common.sql.IqSqlUtils.runSql
 import ca.infoq.spark.common.io.IoUtils.getFileStrFromResource
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
 
 /**
   * Created by infoq.ca(Edward,Wang) on 2016-08-22.
@@ -10,12 +12,17 @@ import ca.infoq.spark.common.io.IoUtils.getFileStrFromResource
 object IqSqlApp {
   def main(args: Array[String]) {
     val confMap = IqAppArgsParser.getArgsMap(args)
-    val app = new IqApp(confMap)
-    val transFile = "src/main/resources/data/fakeTransaction.csv"
+    /*    val app = new IqApp(confMap)
 
-    //scala implicit explained: http://stackoverflow.com/questions/10375633/understanding-implicit-in-scala
-    implicit val sc = app._sc
-    implicit val sqlContext = app._sqlContext
+        //scala implicit explained: http://stackoverflow.com/questions/10375633/understanding-implicit-in-scala
+        implicit val sc = app._sc
+        implicit val sqlContext = app._sqlContext*/
+
+    val conf = new SparkConf().setMaster("local[*]").setAppName("IqSqlApp")
+    implicit val sc = new SparkContext(conf)
+    implicit val sqlContext = new SQLContext(sc)
+
+    val transFile = "src/main/resources/data/fakeTransaction.csv"
 
     //Load sample data
     sqlContext.read.format("com.databricks.spark.csv")
@@ -25,19 +32,17 @@ object IqSqlApp {
 
     //rename method to a shorter name
     def getSqlStr = getFileStrFromResource
-    //setup variable for substitution
-    val sqlVars = Map(("id","1"), ("yearmonth","201607"))
 
     //run sql with pre-defined vars and then registerTempTable
-    runSql(getSqlStr("sql/q1_cust_montly_sum.sql"), sqlVars, "r1")
+    runSql(getSqlStr("sql/q1_cust_montly_sum.sql"), confMap, "r1")
     sqlContext.table("r1").show()
 
     //run another sql from previous result
-    runSql(getSqlStr("sql/q2_select_by_id.sql"), sqlVars, "r2")
+    runSql(getSqlStr("sql/q2_select_by_id.sql"), confMap, "r2")
     sqlContext.table("r2").show()
 
     //Another demo on sql variableSubstitution which similar to hiveconf/hivevar
-    runSql(getSqlStr("sql/q3_filter_by_month.sql"), sqlVars, "r3")
+    runSql(getSqlStr("sql/q3_filter_by_month.sql"), confMap, "r3")
     sqlContext.table("r3").show()
   }
 }
